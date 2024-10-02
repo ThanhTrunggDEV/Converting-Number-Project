314b39a576237f61b7eea32ff9d146e4ddd02437
dovannguyen13102005 <dovannguyen13102005@gmail.com>
2024-10-02 16:32:39 +0700

    new update

--- ConvertingNumber/Form1.cs
+++ ConvertingNumber/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Schema;
 
 namespace ConvertingNumber
 {
@@ -17,7 +19,27 @@ namespace ConvertingNumber
             InitializeComponent();
         }
 
-       public void ConvertToBinary(int value)
+        public string multiply_by_2 (float val)
+        {
+            string s = "";
+            int cnt = 0;
+            while (cnt <= 8)
+            {
+                val *= 2;
+                if (val >= 1)
+                {
+                    s += "1";
+                    val -= 1;
+                }
+                else
+                {
+                    s += "0";
+                }
+                cnt++;
+            }
+            return s;
+        }
+        public string ConvertToBinary(int value)
         {
             List<int> number = new List<int>();
             if (value < 0)
@@ -53,7 +75,7 @@ namespace ConvertingNumber
                     value = value / 2;
                 }
                 number.Reverse();
-                //string s = string.Join("", number);
+                string s = string.Join("", number);
                 number.Reverse();
                 while (number.Count < 32)
                 {
@@ -61,10 +83,11 @@ namespace ConvertingNumber
                 }
                 number.Reverse();
                 textBox2.Text = string.Join("", number);
-                //return s;
+                return s;
             }
+            return "";
         }
-       public int ConvertToDecimal(string binary)
+        public int ConvertToDecimal(string binary)
         {
             double sum = 0;
             int exp = binary.Length - 1;
@@ -75,19 +98,45 @@ namespace ConvertingNumber
             return (int)sum;
         }
 
+        public void ConvertToBinaryforDecimal(float Value)
+        {
+            string s = "";
+            if (Value < 0)
+            {
+                s += "1";
+                Value *= -1;
+            }
+
+            else s += "0";
+            
+
+            int intner = Convert.ToInt32(Value);
+            Value -= intner;
+            string s0 = ConvertToBinary(intner);
+            string s1 = ConvertToBinary(127 + s0.Length - 1);
+            s0 = s0.Remove(0, 1);
+            string s2 = s0 + multiply_by_2(Value);
+            s = s + s1 + s2;
+            for (int i = s.Length - 1; i < 32; i++)
+            {
+                s += "0";
+            }
+            textBox2.Text = s;
+        }
+
        
         #region Caculation Button
         private void button1_Click_1(object sender, EventArgs e)
         {
             textBox2.Text = "";
-            int value;
-            float Value;
+            int value; // value is a interger
+            float Value; // Value is a decimal
             if (!int.TryParse(textBox1.Text, out value) && !float.TryParse(textBox1.Text, out Value))
             {
                 MessageBox.Show("Please enter number!", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else if (!int.TryParse(textBox1.Text, out value) && float.TryParse(textBox1.Text, out Value )) {
-                MessageBox.Show("day la so thap phan");
+                ConvertToBinaryforDecimal(Value);
             }
 
             else
